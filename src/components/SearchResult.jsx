f4401be80480e008ed214e0a0375fc9c8d86093f
Kalpesh Dhoke <kalpeshdhoke7@gmail.com>
2023-11-24 18:40:09 +0530

    implement the err for search functionality during fetching

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -5,16 +5,18 @@ import getAll from '../Services/track';
 
 function SearchResult({ searchSong }) {
   const [data, setData] = useState([]);
+  const [error, setError] = useState(''); 
   
   useEffect(() => {
     getAll()
-    .then(resp => setData(resp.data));
-  }, []);
+    .then(resp => setData(resp.data))
+    .catch(err => setError(err.message))
+  });
 
   return (
     <div className="search-result">
       <h1>Results</h1>
-      <TrackList data={data} searchSong={searchSong} />
+      <TrackList data={data} searchSong={searchSong} error={error} />
     </div>
   )
 }
--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -2,13 +2,16 @@ import Track from './Track'
 
 import '../styles/TrackList.css'
 
-function TrackList({ data, searchSong }) { 
+function TrackList({ data, searchSong, error }) { 
+  const filteredData = data?.filter(track => (
+    track.name.toLowerCase().includes(searchSong.toLowerCase())
+  )) 
+
   return (
     <div className='tracklist'>
+      { error && <p className='error'>{error}</p> }
       { 
-        data &&
-        data.filter(trackSearch => (trackSearch.name.toLowerCase().includes(searchSong)))
-        .map(track => ( 
+        filteredData && filteredData.map(track => ( 
             <Track key={track.id} name={track.name} artist={track.artist} album={track.album}/>
         ))
       }
