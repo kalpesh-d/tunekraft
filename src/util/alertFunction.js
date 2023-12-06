21ff64e4b0b829afecf242474a1553eaf4cbbdf6
Kalpesh Dhoke <kalpeshdhoke7@gmail.com>
2023-12-06 12:27:47 +0530

    update in alert function

--- src/util/alertFunction.js
+++ src/util/alertFunction.js
@@ -1,7 +1,7 @@
 export const alertFunciton = (playlistName, playlistTrack) => {
-  if (playlistName.trim() === "" && playlistTrack.length === 0) {
+  if (!playlistName && playlistTrack.length === 0) {
     return "Please enter a playlist name & add tracks before saving.";
-  } else if (playlistName.trim() === "") {
+  } else if (!playlistName) {
     return "Please enter a playlist name.";
   } else if (playlistTrack.length === 0) {
     return "Your playlist is empty. Add tracks before saving.";
